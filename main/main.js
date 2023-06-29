027502087a708ef76071b8472d738fe1a067051e
N4mys <namyschokusov@gmail.com>
2023-06-29 20:30:36 +0600

    first commit

---
+++ main/main.js
@@ -0,0 +1,62 @@
+//-----------------------&& || !---------------------------------------
+
+// const age = 20;
+// console.log(age < 30 && age > 18);
+
+// console.log(true && false && true);
+
+// console.log(5 < 3 && age > 18 && 5 < 6) // false
+// console.log( false && true && true) // false
+
+// console.log(true || false || flase || false || false); // true odin xyi
+
+// const age = 19;
+// console.log(age > 21 || age > 17); // true
+// console.log(false || true); // example
+// console.log(false || false); // false t.k. oba false
+
+// const isAdmin = false;
+// const txt = "main/js";
+// // const result = false || "main/js"
+// const result = isAdmin || txt;
+
+// console.log(result);
+
+// const book = "Hello";
+// const author = null;
+// const title = "";
+// const result = book || title || "1" || author;
+// console.log(result);
+
+// console.log(false || (true && false)); // false
+// // console.log(("Hello" && false) || (null && true)); // null
+// (true && false) || (null && true);
+// false || null;
+// null;
+
+// console.log("" || (30 && "1" && 10 === 10) || false);
+// "" || (30 && "1" && 10 === 10) || false;
+// false || true || false;
+// true;
+
+// console.log(1234 && "1");
+
+// console.log(!true); // false
+// console.log(!""); // true
+// console.log(!0); // true
+
+// console.log(!!50);
+
+// const stringNumber = "100";
+// console.log(!!stringNumber);
+// console.log(+stringNumber);
+// console.log(+true); // 1
+// console.log(+false); // 0
+// console.log(+undefined); //NaN
+// console.log(+"5gfdhdhsdthdarh"); // Nan
+// console.log(+"gdbrn6"); // NaN
+// console.log(parseInt("6hlksnj;skjrnosrjnosrjns")); // 6
+
+// Приоритетность 1) ! 2) && 3) ||
+// const x = true;
+// const result = x && name;
